1ab724a0a397bf8ba5c458ecab12c2457ca3d015
Aleksejs Popovs <aleksejs.popovs@nccgroup.com>
2019-07-03 15:56:44 -0700

    Package stuff properly

---
+++ requests_racer/__init__.py
@@ -0,0 +1,2 @@
+from .core import SynchronizedAdapter, SynchronizedSession
+from .__version__ import __version__
---
+++ setup.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# based on kennethreitz's example setup.py, which is in public domain
+# https://github.com/kennethreitz/setup.py
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'requests-racer'
+DESCRIPTION = 'A Requests extension for exploiting race conditions in web applications.'
+URL = 'TODO'
+EMAIL = 'aleksejs.popovs@nccgroup.com'
+AUTHOR = 'Aleksejs Popovs'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.0.1'
+
+REQUIRED = [
+    # because this package uses a bunch of hacks that depend on various internal
+    # implementation details of requests and urllib3, here we specifically require
+    # versions that are known to work.
+    'requests==2.22.0',
+    'urllib3==1.25.3',
+]
+
+EXTRAS = {}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+if False:
+    # let's not upload anything anywhere just yet
+    class UploadCommand(Command):
+        """Support setup.py upload."""
+
+        description = 'Build and publish the package.'
+        user_options = []
+
+        @staticmethod
+        def status(s):
+            """Prints things in bold."""
+            print('\033[1m{0}\033[0m'.format(s))
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            try:
+                self.status('Removing previous builds…')
+                rmtree(os.path.join(here, 'dist'))
+            except OSError:
+                pass
+
+            self.status('Building Source and Wheel (universal) distribution…')
+            os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+            self.status('Uploading the package to PyPI via Twine…')
+            os.system('twine upload dist/*')
+
+            self.status('Pushing git tags…')
+            os.system('git tag v{0}'.format(about['__version__']))
+            os.system('git push --tags')
+
+            sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='PROPRIETARY INTERNAL SOFTWARE, DO NOT REDISTRIBUTE',
+    classifiers=[
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'License :: OSI Approved :: MIT License',
+        'License :: Other/Proprietary License',
+        'Intended Audience :: Developers',
+        'Topic :: Security',
+        'Topic :: System :: Networking',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    # cmdclass={
+    #     'upload': UploadCommand,
+    # },
+)
