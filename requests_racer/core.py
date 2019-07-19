ae982b1e947da053125b6b8a9e0b96ffeff5be8c
Aleksejs Popovs <aleksejs.popovs@nccgroup.com>
2019-07-19 12:03:51 -0700

    Add Requests license notice

--- requests_racer/core.py
+++ requests_racer/core.py
@@ -1,3 +1,24 @@
+# This file contains modified parts of code from Kenneth Reitz's Requests,
+# available at https://github.com/kennethreitz/requests.
+# The Requests code (but not the modifications present in Requests-Racer)
+# is distributed under the following license:
+#
+# [BEGIN REQUESTS LICENSE]
+# Copyright 2018 Kenneth Reitz
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+# [END REQUESTS LICENSE]
+
 import socket
 import sys
 import threading
