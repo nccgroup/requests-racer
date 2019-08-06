4cf45488059e075aac4515f995fb8b6d5c0c616f
Aleksejs Popovs <aleksejs.popovs@nccgroup.com>
2019-08-06 13:58:38 -0700

    Add license

--- requests_racer/core.py
+++ requests_racer/core.py
@@ -1,3 +1,19 @@
+# Requests-Racer, a Python library for synchronizing HTTP requests.
+# Copyright (C) 2019 Aleksejs Popovs, NCC Group
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 # This file contains modified parts of code from the Requests library,
 # in particular the file requests/adapters.py as of commit
 # 7fd9267b3bab1d45f5e4ac0953629c5531ecbc55. The Requests library is
