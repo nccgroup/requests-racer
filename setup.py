81ecb9813bd1506437a4a1d80f7b96497874efa3
Aleksejs Popovs <aleksejs.popovs@nccgroup.com>
2019-08-07 15:07:40 -0700

    Reenable publishing functionality in setup.py

--- setup.py
+++ setup.py
@@ -48,43 +48,41 @@ else:
     about['__version__'] = VERSION
 
 
-if False:
-    # let's not upload anything anywhere just yet
-    class UploadCommand(Command):
-        """Support setup.py upload."""
+class UploadCommand(Command):
+    """Support setup.py upload."""
 
-        description = 'Build and publish the package.'
-        user_options = []
+    description = 'Build and publish the package.'
+    user_options = []
 
-        @staticmethod
-        def status(s):
-            """Prints things in bold."""
-            print('\033[1m{0}\033[0m'.format(s))
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
 
-        def initialize_options(self):
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
             pass
 
-        def finalize_options(self):
-            pass
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
 
-        def run(self):
-            try:
-                self.status('Removing previous builds…')
-                rmtree(os.path.join(here, 'dist'))
-            except OSError:
-                pass
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
 
-            self.status('Building Source and Wheel (universal) distribution…')
-            os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
 
-            self.status('Uploading the package to PyPI via Twine…')
-            os.system('twine upload dist/*')
-
-            self.status('Pushing git tags…')
-            os.system('git tag v{0}'.format(about['__version__']))
-            os.system('git push --tags')
-
-            sys.exit()
+        sys.exit()
 
 
 # Where the magic happens:
@@ -119,7 +117,7 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
     # $ setup.py publish support.
-    # cmdclass={
-    #     'upload': UploadCommand,
-    # },
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
